44471cf217a522c7f2a63806daa57532cdb0c413
GwenP88 <12777@holbertonstudents.com>
2026-02-24 12:51:21 +0100

    rename test_models for just amenity

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,16 +1,33 @@
 from basemodel import BaseModel
+from werkzeug.security import generate_password_hash
 
 class User(BaseModel):
 
     _storage = {}
 
-    def __init__(self, first_name, last_name, email, password, is_admin = False):
+    def __init__(self, first_name, last_name, email, is_admin = False):
         super().__init__()
         self._first_name = first_name
         self._last_name = last_name
         self._email = email
-        self._password = password
+        self._password = None
         self._is_admin = is_admin
+
+    @property
+    def first_name(self):
+        return self._first_name
+    
+    @property
+    def last_name(self):
+        return self._last_name
+    
+    @property
+    def is_admin(self):
+        return self._is_admin
+
+    @property
+    def email(self):
+        return self._email
 
     @staticmethod
     def _validate_first_name(first_name):
@@ -25,15 +42,106 @@ class User(BaseModel):
             raise ValueError("last_name is required and must be a non-empty string.")
         if len(last_name) > 50:
             raise ValueError("last_name must not exceed 50 characters.")
+        
+    @staticmethod
+    def _validate_email(email):
+        if email is None or not isinstance(email, str):
+            raise ValueError("email is required and must be a string.")
+        email = email.strip().lower()
+        if not email:
+            raise ValueError("email must not be empty")
+        if email.count("@") != 1:
+            raise ValueError("Email must contain exactly one '@'.")
+        return email
 
-    def create_user():
+    @staticmethod
+    def _validate_password(password):
+        if password is None or not isinstance(password, str):
+            raise ValueError("password is required and must be a string.")
+        pswd = password.strip()
+        if not pswd:
+            raise ValueError("password must not be empty")
+        if len(pswd) < 8:
+            raise ValueError("password must have 8 characters min.")
+        has_letter = any(letter.isalpha() for letter in pswd)
+        has_digit = any(digit.isdigit() for digit in pswd)
+        if not has_letter or not has_digit:
+            raise ValueError("password must have 1 letter and 1 digit")
 
-    def get_all_users():
+    @classmethod
+    def _email_exists(cls, email):
+        for user in cls._storage.values():
+            if user.email == email:
+                return True
+        return False
+    
+    def set_password(self, password):
+        self._validate_password(password)
+        self._password = generate_password_hash(password.strip())
+        self.save()
+    
+    @classmethod
+    def create_user(cls, data):
+        if not data or not isinstance(data, dict):
+            raise ValueError("User data must be a non-empty dictionary")
+        if "is_admin" in data:
+            raise ValueError("Only an administrator can set is_admin.")
+        first_name = data.get("first_name", "")
+        last_name = data.get("last_name", "")
+        email = data.get("email", "")
+        password = data.get("password", "")
+        cls._validate_first_name(first_name)
+        cls._validate_last_name(last_name)
+        email = cls._validate_email(email)
+        if cls._email_exists(email): 
+            raise ValueError("Email already exists")
+        cls._validate_password(password)
+        user = cls(first_name, last_name, email, is_admin=False)
+        user.set_password(password)
+        cls._storage[user.id] = user
+        return user
 
-    def get_profil():
+    @classmethod
+    def get_all_users(cls):
+        list_users = []
+        for u in cls._storage.values():
+            list_users.append(u.get_profile())
+        return list_users
 
-    def update_user():
+    def get_profile(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "is_admin": self.is_admin,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
 
-    def set_password():
+    def update_user(self, data):
+        if not data or not isinstance(data, dict):
+            raise ValueError("No data to update")
+        if "is_admin" in data:
+            raise ValueError("Only an administrator can set is_admin.")
+        if "first_name" in data:
+            self._validate_first_name(data["first_name"])
+            self._first_name = data["first_name"]
+        if "last_name" in data:
+            self._validate_last_name(data["last_name"])
+            self._first_name = data["last_name"]
+        if "email" in data:
+            new_email = self._validate_email(data["email"])
+            if new_email != self.email:
+                if User._email_exists(new_email):
+                    raise ValueError("Email already exists")
+                self._email = new_email
+        if "password" in data:
+            self.set_password(data["password"])
+        self.save()
 
-    def delete():
+    def delete(self):
+        storage = self.__class__._storage
+        if self.id not in storage:
+            raise ValueError("User not found in storage.")
+        del storage[self.id]
