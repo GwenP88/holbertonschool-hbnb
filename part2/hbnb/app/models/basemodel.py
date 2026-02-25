7a0389b37b7f8c0ff582295af2d3a50ec91cebea
GwenP88 <12777@holbertonstudents.com>
2026-02-25 15:06:04 +0100

    feat: add generic update() method to BaseModel to support repository updates

--- part2/hbnb/app/models/basemodel.py
+++ part2/hbnb/app/models/basemodel.py
@@ -6,21 +6,33 @@ class BaseModel:
         self._id = str(uuid.uuid4())
         self._created_at = datetime.now()
         self._updated_at = datetime.now()
-    
+
     @property
     def id(self):
         return self._id
-    
+
     @property
     def created_at(self):
         return self._created_at
-    
+
     @property
     def updated_at(self):
         return self._updated_at
 
     def update_time(self):
         self._updated_at = datetime.now()
-        
+
     def save(self):
         self.update_time()
+
+    def update(self, data):
+        if not isinstance(data, dict) or not data:
+            raise ValueError("Update data must be a non-empty dictionary")
+        protected_fields = {"id", "created_at", "updated_at"}
+        for key, value in data.items():
+            if key in protected_fields:
+                continue
+            attr = "_" + key
+            if hasattr(self, attr):
+                setattr(self, attr, value)
+        self.save()
