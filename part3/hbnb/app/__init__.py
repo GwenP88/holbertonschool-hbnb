bd48bc4f05e77b221a105c63deb2f59bce665f41
GwenP88 <12777@holbertonstudents.com>
2026-03-09 12:24:55 +0100

    feat: update Flask application factory to support configuration classes

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -7,15 +7,16 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
 
-def create_app():
-    """Create and configure the Flask app and register API namespaces."""
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
+
     api = Api(
         app, version='1.0',
         title='HBnB API',
         description='HBnB Application API',
         doc='/api/v1/'
-        )
+    )
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
