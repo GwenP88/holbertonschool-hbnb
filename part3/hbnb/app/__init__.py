c5e83ad82502a01ea30e50c497eed84a7a3d29a3
GwenP88 <12777@holbertonstudents.com>
2026-03-11 15:16:43 +0100

    map User entity to SQLAlchemy model and implement UserRepository with facade integration

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,17 +2,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt()
-
 from flask_jwt_extended import JWTManager
-jwt = JWTManager()
-
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.auth import api as auth_ns
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
+db = SQLAlchemy()
 
 db = SQLAlchemy()
 
@@ -40,6 +35,12 @@ def create_app(config_class="config.DevelopmentConfig"):
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.auth import api as auth_ns
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
