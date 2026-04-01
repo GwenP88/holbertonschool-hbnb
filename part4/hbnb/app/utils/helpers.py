4ee5b1b34efecf4ffd3184d74312687181960984
GwenP88 <12777@holbertonstudents.com>
2026-04-01 10:48:01 +0200

    feat: complete task 0 - HTML/CSS design for all pages with W3C validation

--- part4/hbnb/app/utils/helpers.py
+++ part4/hbnb/app/utils/helpers.py
@@ -17,15 +17,15 @@ def get_city_name(latitude, longitude):
 # --- Image resolution ---
 
 PLACE_IMAGES = {
-    'ef448d99-36e6-4aa6-880a-59bab9bbe439': 'images/places/place_1.jpg',
-    'b7f90bd1-3466-4429-b57d-e00adb798bbc': 'images/places/place_2.jpg',
-    '57ea271c-e55e-4a4e-94cb-ab5319664e7e': 'images/places/place_3.jpg',
-    '69354fdc-70f9-403a-9f88-c77147087909': 'images/places/place_4.jpg',
-    '5fcc6d25-3bf1-4701-b8dc-a8016def143d': 'images/places/place_5.jpg',
-    '9bc8b8f5-ac16-4ce3-b3b1-91b1b30d934d': 'images/places/place_6.jpg',
+    'ef448d99-36e6-4aa6-880a-59bab9bbe439': 'images/places/place_1.webp',
+    'b7f90bd1-3466-4429-b57d-e00adb798bbc': 'images/places/place_2.webp',
+    '57ea271c-e55e-4a4e-94cb-ab5319664e7e': 'images/places/place_3.webp',
+    '69354fdc-70f9-403a-9f88-c77147087909': 'images/places/place_4.webp',
+    '5fcc6d25-3bf1-4701-b8dc-a8016def143d': 'images/places/place_5.webp',
+    '9bc8b8f5-ac16-4ce3-b3b1-91b1b30d934d': 'images/places/place_6.webp',
 }
 
-DEFAULT_IMAGE = 'images/places/place_default.png'
+DEFAULT_IMAGE = 'images/places/place_default.webp'
 
 def get_place_image(place_id):
     """Return the image path for a place, or a default image if unknown."""
